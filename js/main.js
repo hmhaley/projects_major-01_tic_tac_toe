f61acff97d23e76e7ea7274bd234d59f6a4eaf02
H. Martin Haley <hmhaleyjr@Martin-Air.local>
2014-10-10 15:52:57 -0700

    added my angular app to the js file and my first array of variables for 'players'

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var guiClonedWarsApp = angular.module('guiClonedWarsApp', []);
+	guiClonedWarsApp.controller('GuiClonedWarsController', function ($scope){
+
+		$scope.players = [
+			{
+				firstName: 'Player1FirstName',
+				lastName: 'Player1LastName',
+				ceoName: 'Steve Jobs',
+				ceoImage: "../images/jobs.jpg",
+				ceoSound: ,
+				starWarsName: 'Yoda',
+				starWarsImage: ,
+				starWarsSound: ,
+				move1: ,
+				move2: ,
+				move3: ,
+				move4: ,
+				move5: ,
+				winOrLose: ,
+			},
+					{
+				firstName: 'Player2FirstName',
+				lastName: 'Player2LastName',
+				ceoName: 'Bill Gates',
+				ceoImage: "../images/gates.jpg",
+				ceoSound: ,
+				starWarsName: 'Vader',
+				starWarsImage: ,
+				starWarsSound: ,
+				move1: ,
+				move2: ,
+				move3: ,
+				move4: ,
+				move5: ,
+				winOrLose: ,
+			}
+
+		]
+
+});
