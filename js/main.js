700e1deb4fd5a7a7632b4c10caa61c8b774e49f3
H. Martin Haley <hmhaleyjr@Martin-Air.local>
2014-10-15 16:10:58 -0700

    fixed the logic and flushed it out for all winning possibilities. got all of the logic to work and console log the correct answers; have met minimum reqs for what I can do so far

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-var guiClonedWarsApp = angular.module('guiClonedWarsApp', []);
-	guiClonedWarsApp.controller('GuiClonedWarsController', function ($scope, $http){
+var guiClonedWarsApp = angular.module('guiClonedWarsApp', ['firebase']);
+	guiClonedWarsApp.controller('GuiClonedWarsController', function ($scope, $firebase){
+
+		var guiClonedDatabase = new Firebase("https://guiclonedwars.firebaseio.com");
 
 		// $scope.players = [
 		// 	{
@@ -37,36 +39,142 @@ var guiClonedWarsApp = angular.module('guiClonedWarsApp', []);
 
 		// ] ;
 
-		  $scope.cellList = [
-		  {xostatus: "0"}, 
-		  {xostatus: "1"}, 
-		  {xostatus: "2"}, 
-		  {xostatus: "3"}, 
-		  {xostatus: "4"}, 
-		  {xostatus: "5"}, 
-		  {xostatus: "6"}, 
-		  {xostatus: "7"}, 
-		  {xostatus: "8"}
+		$scope.allMoves = $firebase(guiClonedDatabase);
+
+		$scope.addMoves = function() { 
+		  //Add manually using standard JavaScript
+		  guiClonedDatabase.push( {moveCounter:$scope.moveCounter} );
+		  $scope.moveCounter;
+		  };
+
+
+		$scope.cellList = [
+		{cellLocation: "0", xostatus: "not yet played"}, 
+		{cellLocation: "1", xostatus: "not yet played"}, 
+		{cellLocation: "2", xostatus: "not yet played"}, 
+		{cellLocation: "3", xostatus: "not yet played"}, 
+		{cellLocation: "4", xostatus: "not yet played"}, 
+		{cellLocation: "5", xostatus: "not yet played"}, 
+		{cellLocation: "6", xostatus: "not yet played"}, 
+		{cellLocation: "7", xostatus: "not yet played"}, 
+		{cellLocation: "8", xostatus: "not yet played"}
 		  ]  ;
 
 		  $scope.movecounter = 0 ;
 
-		  $scope.testJS = function() {
-		    console.log('Correctly accessing JS function.') ;
-		  } ;
-
 		  $scope.playerPicks = function(thisCell) {
 		    $scope.movecounter = $scope.movecounter + 1 ;
-		    console.log("Cell was: " + thisCell.xostatus) ;
+		    console.log("Cell " + thisCell.cellLocation + "\'s status was \'" + thisCell.xostatus + "\'") ;
 		    if (($scope.movecounter % 2) == 1) {
 		      thisCell.xostatus = "X" ;  
 		    } else {
 		      thisCell.xostatus = "O" ;
 		    } 
-		    console.log("Cell is now: " + thisCell.xostatus) ;
+		    console.log("Cell " + thisCell.cellLocation + "\'s status now is \'" + thisCell.xostatus + "\'") ;
+		    $scope.checkForWinner()
 		  } ;
 
+		$scope.checkForWinner = function() {
+		  	if (
+		  		(
+		  			($scope.cellList[0].xostatus == "X") && 
+		  			($scope.cellList[1].xostatus == "X") && 
+		  			($scope.cellList[2].xostatus == "X")
+		  		)
+		  		|| 
+		  		(
+		  			($scope.cellList[3].xostatus == "X") && 
+		  			($scope.cellList[4].xostatus == "X") && 
+		  			($scope.cellList[5].xostatus == "X")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[6].xostatus == "X") && 
+		  			($scope.cellList[7].xostatus == "X") && 
+		  			($scope.cellList[8].xostatus == "X")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[0].xostatus == "X") && 
+		  			($scope.cellList[3].xostatus == "X") && 
+		  			($scope.cellList[6].xostatus == "X")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[1].xostatus == "X") && 
+		  			($scope.cellList[4].xostatus == "X") && 
+		  			($scope.cellList[7].xostatus == "X")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[2].xostatus == "X") && 
+		  			($scope.cellList[5].xostatus == "X") && 
+		  			($scope.cellList[8].xostatus == "X")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[0].xostatus == "X") && 
+		  			($scope.cellList[4].xostatus == "X") && 
+		  			($scope.cellList[8].xostatus == "X")
+		  		)
+		  	){
+		  		console.log("X is the winner");
+		  	}
+		  	else if (
+		  		(
+		  			($scope.cellList[0].xostatus == "O") && 
+		  			($scope.cellList[1].xostatus == "O") && 
+		  			($scope.cellList[2].xostatus == "O")
+		  		)
+		  		|| 
+		  		(
+		  			($scope.cellList[3].xostatus == "O") && 
+		  			($scope.cellList[4].xostatus == "O") && 
+		  			($scope.cellList[5].xostatus == "O")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[6].xostatus == "O") && 
+		  			($scope.cellList[7].xostatus == "O") && 
+		  			($scope.cellList[8].xostatus == "O")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[0].xostatus == "O") && 
+		  			($scope.cellList[3].xostatus == "O") && 
+		  			($scope.cellList[6].xostatus == "O")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[1].xostatus == "O") && 
+		  			($scope.cellList[4].xostatus == "O") && 
+		  			($scope.cellList[7].xostatus == "O")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[2].xostatus == "O") && 
+		  			($scope.cellList[5].xostatus == "O") && 
+		  			($scope.cellList[8].xostatus == "O")
+		  		)
+		  		||
+		  		(
+		  			($scope.cellList[0].xostatus == "O") && 
+		  			($scope.cellList[4].xostatus == "O") && 
+		  			($scope.cellList[8].xostatus == "O")
+		  		)
+		  	){
+		  		console.log("O is the winner");
+		  	}
+		  	else {
+		  		console.log("These Guys are in a fight to a Tie!");
+		    }
+	    };
+
 });
+
+// function checkForWinnerFunction() {
+//     document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+// }
 
 // function playSound(soundfile) {
 //   document.getElementsbyClassName("xclass").innerHTML= "<embed src=\""
